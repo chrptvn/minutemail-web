330e3246e90b6079b5d4dc18ea010db4350532f3
chrptvn <31348581+chrptvn@users.noreply.github.com>
2025-07-09 22:37:13 -0400

    Update manage domain page icons

--- src/app/shared/components/icons/tabler-icons.component.ts
+++ src/app/shared/components/icons/tabler-icons.component.ts
@@ -279,6 +279,22 @@ import { Component, Input } from '@angular/core';
           </g>
         }
       }
+        @case('globe') {
+          <g>
+            <circle cx="12" cy="12" r="10"/>
+            <path d="M12 2a14.5 14.5 0 0 0 0 20 14.5 14.5 0 0 0 0-20"/>
+            <path d="M2 12h20"/>
+          </g>
+        }
+
+        @case('dns') {
+          <g>
+            <circle cx="12" cy="12" r="10"/>
+            <path d="M12 2a14.5 14.5 0 0 0 0 20 14.5 14.5 0 0 0 0-20"/>
+            <path d="M2 12h20"/>
+            <path d="M12 2v20"/>
+          </g>
+        }
     </svg>
   `
 })
