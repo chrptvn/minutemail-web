4b2608a75090a414290513a601a34e112a3b2525
Christian Potvin <christian.potvin@vanderlande.com>
2025-07-09 23:15:00 -0400

    fix

--- src/app/pages/mx-configuration/mx-configuration.component.ts
+++ src/app/pages/mx-configuration/mx-configuration.component.ts
@@ -2,14 +2,14 @@ import { Component, OnInit, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ThemeService } from '../../core/services/theme.service';
-import { ButtonComponent } from '../../shared/components/ui/button.component';
 import { TablerIconComponent } from '../../shared/components/icons/tabler-icons.component';
 import { ProfileMenuComponent } from '../../shared/components/profile-menu/profile-menu.component';
+import {ButtonComponent} from '../../shared/components/ui/button.component';
 
 @Component({
   selector: 'app-mx-configuration',
   standalone: true,
-  imports: [CommonModule, ButtonComponent, TablerIconComponent, ProfileMenuComponent],
+  imports: [CommonModule, TablerIconComponent, ProfileMenuComponent, ButtonComponent],
   templateUrl: './mx-configuration.component.html',
   styleUrl: './mx-configuration.component.scss'
 })
